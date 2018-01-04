e082daf10b5dbd6316cffc860ca9cf05e80ec848
Jeff Buttars <jeffbuttars@gmail.com>
2018-01-04 11:20:52 -0700

    Add semantic libs and tachyons link

--- src/App.js
+++ src/App.js
@@ -1,29 +1,31 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 import { Provider } from 'react-redux'
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import 'semantic-ui-css/semantic.min.css'
+import Layout from 'layout'
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
-}
+// class App extends Component {
+//   render() {
+//     return (
+//       <div className="App">
+//         <header className="App-header">
+//           <img src={logo} className="App-logo" alt="logo" />
+//           <h1 className="App-title">Welcome to React</h1>
+//         </header>
+//         <p className="App-intro">
+//           To get started, edit <code>src/App.js</code> and save to reload.
+//         </p>
+//       </div>
+//     );
+//   }
+// }
 
 export default ({ store }) => (
   <Provider store={store}>
     <Router>
-      <Route component={App} />
+      <Route component={Layout} />
     </Router>
   </Provider>
 )
