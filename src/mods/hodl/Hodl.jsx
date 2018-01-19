15c9b7abcebc86bea24a27b04935053340920cb3
Jeff Buttars <jeffbuttars@gmail.com>
2018-01-19 12:51:19 -0700

    Refactor the high level chart into Stock Chart

--- src/mods/hodl/Hodl.jsx
+++ src/mods/hodl/Hodl.jsx
@@ -1,23 +1,13 @@
 import React from 'react'
-import { List, OrderedMap } from 'immutable'
+import { OrderedMap } from 'immutable'
 import { Dropdown, Checkbox } from 'semantic-ui-react'
 import Reduxer from 'comp-builder/reduxer'
-import moment from 'moment'
-import {
-  VictoryChart,
-  VictoryAxis,
-  // VictoryCursorContainer,
-  VictoryVoronoiContainer,
-  VictoryTooltip,
-  VictoryLabel,
-  VictoryCandlestick
-} from 'victory'
-import Regressions, {regressionColors} from './Regressions'
-import DataLines, {dataLineColors} from './DataLines'
+import {regressionColors} from './Regressions'
+import {dataLineColors} from './DataLines'
 import * as hodlActions from './actions'
+import StockChart from './StockChart'
 
 // XXX Need moving averages and resistance lines
-
 
 const regressionOptions = [
   {key: 'combined', text: 'Combined', value: 'combined'},
@@ -49,18 +39,6 @@ const regressionsLabel = (label) => {
     },
     content: label.text
   }
-}
-
-const chartDataLabel = (data) => {
-  if (data.childName !== 'candlestick') {
-    return
-  }
-
-  return `${moment(data.time).format('M/D')}\nOpen ${data.open}\nClose ${data.close}\nLow ${data.low}\nHigh ${data.high}`
-}
-
-const ChartDataTooltip = (props) => {
-  return (<VictoryTooltip {...props} style={{}}/>)
 }
 
 class Hodl extends React.Component {
@@ -135,38 +113,9 @@ class Hodl extends React.Component {
     actions.hodl.getDailyHistory('ETH', '90')
   }
 
-  getYAxis = (data) => {
-    const allValues = data.reduce((p, v) => {
-      return p.concat(v.get('close'), v.get('open'), v.get('low'), v.get('high'))
-    }, List())
-
-    // allValues.push(Math.floor(allValues.get(0) / 100) * 100)
-    // console.log('getYAxis allValues', allValues.toJS())
-    const minVal = Math.floor(allValues.min() / 100) * 100
-    const maxVal = Math.ceil(allValues.max() / 100) * 100
-
-    const diff = maxVal - minVal
-    const step = diff / 5
-
-    const axis = []
-    for (let i = minVal; i <= maxVal; i += step) {
-      axis.push(i)
-    }
-
-    return axis
-  }
-
-  onChartHover ({datum}) {
-    this.setState(() => ({cursorData: datum}))
-  }
-
   render () {
     const {hodl} = this.props
     let data = hodl.getIn(['ETH', 'byDay'], OrderedMap())
-    const dataArray = data.reduce((p, v) => p.concat(v.toJS()), [])
-    // Should cache in state
-    // console.log('HODL!!!!', data.toJS())
-    // console.log('HODL dataArray', dataArray)
 
     return (
       <div>
@@ -210,93 +159,7 @@ class Hodl extends React.Component {
                   </div>
                 </div>
               </div>
-              <VictoryChart
-                scale="time"
-                domainPadding={{x: 15}}
-                animate={{duration: 1000}}
-                events={[{
-                  childName: 'all',
-                  target: 'data',
-                  eventHandlers: {
-                    onClick: (ev, value) => this.onChartHover( value),
-                    onMouseOver: (ev, value) => this.onChartHover(value)
-                  }
-                }]}
-                containerComponent={
-                  <VictoryVoronoiContainer
-                    textAnchor='left'
-                    labelComponent={
-                      <VictoryTooltip
-                        cornerRadius={3}
-                        className='flex tl'
-                        style={{fontSize: 5, strokeWidth: 0.5, strokeOpacity: 0.5, textAnchor: 'left'}}
-                        flyoutStyle={{strokeWidth: 0, fill: '#FFF', fillOpacity: 0.8, filter: 'url(#blurMe)'}}
-
-                      />
-                      }
-                    voronoiDimension="x"
-                    labels={chartDataLabel}
-                  />
-                  }
-              >
-                <filter id="blurMe">
-                  <feGaussianBlur in='StrokePaint' stdDeviation="1" />
-                </filter>
-
-                <VictoryAxis
-                  tickValues={dataArray.map(t => t.time)}
-                  tickFormat={t => moment(t).format('M/D')}
-                  tickCount={data.size}
-                  fixLabelOverlap={true}
-                  style={{
-                    axis: {
-                      stroke: '#ddd'
-                    },
-                    tickLabels: {
-                      fontSize: 4
-                    }
-                  }}
-                />
-
-                <VictoryAxis
-                  dependentAxis
-                  scale="linear"
-                  tickValues={this.getYAxis(data)}
-                  tickFormat={t => `${Math.round(t)}`}
-                  fixLabelOverlap={true}
-                  style={{
-                    grid: {
-                      strokeWidth: 1,
-                      stroke: '#EEE'
-                    },
-                    axis: {
-                      stroke: '#ddd'
-                    },
-                    tickLabels: {
-                      fontSize: 4
-                    }
-                  }}
-                />
-
-                {this.state.candlestick &&
-                  <VictoryCandlestick
-                    name='candlestick'
-                    candleColors={{positive: '#3A3', negative: '#c43a31'}}
-                    data={dataArray}
-                    x='time'
-                    style={{
-                      data: {
-                        strokeWidth: 0.75,
-                        strokeOpacity: 0.5,
-                        fillOpacity: 0.8
-                      }
-                    }}
-                  />
-                }
-                <Regressions name='regressionLines' data={data} {...this.state.regressions} />
-                <DataLines name='dataLines' data={data} {...this.state.dataLines} />
-              </VictoryChart>
-
+              <StockChart data={data} {...this.state} />
             </div>
           : <div></div>
         }
---
+++ src/mods/hodl/StockChart.jsx
@@ -0,0 +1,133 @@
+import React from 'react'
+import { List } from 'immutable'
+import moment from 'moment'
+import {
+  VictoryChart,
+  VictoryAxis,
+  VictoryVoronoiContainer,
+  VictoryTooltip,
+  VictoryCandlestick
+} from 'victory'
+import Regressions from './Regressions'
+import DataLines from './DataLines'
+
+const getYAxis = (data) => {
+  const allValues = data.reduce((p, v) => {
+    return p.concat(v.get('close'), v.get('open'), v.get('low'), v.get('high'))
+  }, List())
+
+  // allValues.push(Math.floor(allValues.get(0) / 100) * 100)
+  const minVal = Math.floor(allValues.min() / 100) * 100
+  const maxVal = Math.ceil(allValues.max() / 100) * 100
+
+  const diff = maxVal - minVal
+  const step = diff / 5
+
+  const axis = []
+  for (let i = minVal; i <= maxVal; i += step) {
+    axis.push(i)
+  }
+
+  return axis
+}
+
+const chartDataLabel = (data) => {
+  if (data.childName !== 'candlestick') {
+    return
+  }
+
+  const lines = [
+    moment(data.time).format('M/D'),
+    `O ${data.open}`,
+    `C ${data.close}`,
+    `L ${data.low}`,
+    `H ${data.high}`
+  ]
+
+  return lines.join('\n')
+}
+
+export default ({data, candlestick, regressions, dataLines}) => {
+  const dataArray = data.reduce((p, v) => p.concat(v.toJS()), [])
+
+  return (
+    <VictoryChart
+      scale="time"
+      domainPadding={{x: 15}}
+      animate={{duration: 1000}}
+      containerComponent={
+        <VictoryVoronoiContainer
+          standalone='true'
+          textAnchor='left'
+          labelComponent={
+            <VictoryTooltip
+              style={{fontSize: 5, strokeWidth: 0.5, strokeOpacity: 0.5}}
+              flyoutStyle={{strokeWidth: 0, fill: '#FFF', fillOpacity: 0.8, filter: 'url(#blurMe)'}}
+
+            />
+            }
+            voronoiDimension="x"
+            labels={chartDataLabel}
+          />
+      }
+    >
+      <filter id="blurMe">
+        <feGaussianBlur in='StrokePaint' stdDeviation="1" />
+      </filter>
+
+      <VictoryAxis
+        tickValues={dataArray.map(t => t.time)}
+        tickFormat={t => moment(t).format('M/D')}
+        tickCount={data.size}
+        fixLabelOverlap={true}
+        style={{
+          axis: {
+            stroke: '#ddd'
+          },
+          tickLabels: {
+            fontSize: 4
+          }
+        }}
+      />
+
+    <VictoryAxis
+      dependentAxis
+      scale="linear"
+      tickValues={getYAxis(data)}
+      tickFormat={t => `${Math.round(t)}`}
+      fixLabelOverlap={true}
+      style={{
+        grid: {
+          strokeWidth: 1,
+          stroke: '#EEE'
+        },
+        axis: {
+          stroke: '#ddd'
+        },
+        tickLabels: {
+          fontSize: 4
+        }
+      }}
+    />
+
+    {candlestick &&
+    <VictoryCandlestick
+      name='candlestick'
+      candleColors={{positive: '#3A3', negative: '#c43a31'}}
+      data={dataArray}
+      x='time'
+      style={{
+        data: {
+          strokeWidth: 0.75,
+          strokeOpacity: 0.5,
+          fillOpacity: 0.8
+        }
+      }}
+    />
+    }
+
+    <Regressions name='regressionLines' data={data} {...regressions} />
+    <DataLines name='dataLines' data={data} {...dataLines} />
+  </VictoryChart>
+)
+}
